874f2a6a696071314ac3d03e667ffdd899da2c8e
lucydct <tenalucia0@gmail.com>
2023-06-01 21:09:33 +0200

    feat: navBar link / btn styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Repositories() {
       <form className="w-11/12">
         <div className="relative w-full" >
           <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Repositories.." required />
-          <button type="submit" className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <button
+            className='absolute top-0 right-0 p-2.5 text-sm font-medium text-stone-200 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800'
+          >
             <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             <span className="sr-only">Search</span>
           </button>
--- src/components/NavBar/components/NavBar.tsx
+++ src/components/NavBar/components/NavBar.tsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
 	return (
 		<nav className="bg-white border-gray-200 dark:bg-gray-900 flex flex-wrap items-center justify-between py-4 px-6">
 			<span>
-				<a href="https://flowbite.com/" className="flex items-center">
+				<a href="./" className="flex items-center">
 					<Image src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" width={32} height={32} />
 					<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
 				</a>
