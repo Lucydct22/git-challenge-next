6d21d49010c4bfe870531e6241d2cda5bd50158b
lucydct <tenalucia0@gmail.com>
2023-06-01 21:51:45 +0200

    feat: logo

--- src/components/NavBar/components/NavBar.tsx
+++ src/components/NavBar/components/NavBar.tsx
@@ -2,13 +2,14 @@ import Image from "next/image"
 import Link from "next/link"
 import { NavBarButton } from "./NavBarButton"
 
+
 export const NavBar = () => {
 	return (
 		<nav className="bg-white border-gray-200 dark:bg-gray-900 flex flex-wrap items-center justify-between py-4 px-6">
 			<span>
 				<a href="./" className="flex items-center">
-					<Image src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" width={32} height={32} />
-					<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+					<Image src="" className="h-8 mr-3" alt="Logo" width={32} height={32} />
+					<span className="self-center text-2xl font-semibold text-blue-400 dark:text-white">RepoTrack</span>
 				</a>
 			</span>
 
@@ -20,3 +21,4 @@ export const NavBar = () => {
 		</nav>
 	)
 }
+
