4cdbfb4c17e9f216dd73ed56404c4f4cc50e43cf
lucydct <tenalucia0@gmail.com>
2023-06-01 22:34:02 +0200

    Feat: SideBar correction

--- src/components/SideBar/components/SideBar.tsx
+++ src/components/SideBar/components/SideBar.tsx
@@ -9,10 +9,11 @@ export const SideBar = () => {
 					<div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
 						<svg className="w-12 h-12 text-gray-200 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" /></svg>
 					</div>
-					<h1 className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4">Lucy dct</h1>
-					<h2 className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5">Front-end developer</h2>
-					<p className="mb-3 text-gray-500 dark:text-gray-400">As a student at the Assemble Institute of Technology Assemble Institute of Technology, I have learned numerous technologies, including <strong className="font-semibold text-gray-900 dark:text-white">JavaScript, GIT, PHP, SQL, React, and Node.js.</strong> 	What are my interests? 💻 Software Development | 🧠 Marketing | 🎯 SEO | ✈️ Traveling  and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes. </p>
-					<ul>
+					<h2 className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4">Lucy dct</h2>
+					<h3 className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5">Front-end developer</h3>
+					<p className="mb-3 text-gray-500 dark:text-gray-400">I use technologies, including <strong className="font-semibold text-gray-900 dark:text-white">JavaScript, GIT, PHP, SQL, React, TypeScript, Node.js...</strong></p>
+					<p> 💻 Software Development | 🧠 Marketing | 🎯 SEO | ✈️ Traveling</p>
+					<ul >
 						<button
 							className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800'
 						>Following</button>
